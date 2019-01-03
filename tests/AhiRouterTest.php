5929dc8a970eb2f4cc3484537e047e639736bd0b
bmf-san <bmf.infomation@gmail.com>
2019-01-03 20:59:33 +0900

    [update]

--- tests/AhiRouterTest.php
+++ tests/AhiRouterTest.php
@@ -5,7 +5,49 @@ namespace bmfsan\AhiRouterTest;
 use PHPUnit\Framework\TestCase;
 use bmfsan\AhiRouter;
 
-class AhiRouterTest extends TestCase {
+class AhiRouterTest extends TestCase 
+{
+  private $routes = [
+      '/' => [
+          'SLASH_NODE' => [
+              'GET' => 'PATH: / METHOD: GET',
+          ],
+          'users' => [
+              'SLASH_NODE' => [
+                  'GET' => 'PATH: /users METHOD: GET',
+              ],
+              ':user_id' => [
+                  'SLASH_NODE' => [
+                      'GET' => 'PATH: /users/:user_id METHOD: GET',
+                      'POST' => 'PATH: /users/:user_id METHOD: POST',
+                  ],
+                  'events' =>  [
+                      'SLASH_NODE' => [
+                          'GET' => 'PATH: /users/:user_id/events METHOD: GET',
+                      ],
+                      ':event_id' => [
+                          'SLASH_NODE' => [
+                              'GET' => 'PATH: /users/:user_id/events/:event_id METHOD: GET',
+                              'POST' => 'PATH: /users/:user_id/events/:event_id METHOD: POST',
+                          ],
+                      ],
+                  ],
+              ],
+              ':event_id' => [
+                  'SLASH_NODE' => [
+                      'GET' => 'PATH: /users/:event_id METHOD: GET',
+                      'POST' => 'PATH: /users/:event_id METHOD: POST',
+                  ],
+              ],
+              'support' => [
+                  'SLASH_NODE' => [
+                      'GET' => 'PATH: /users/support METHOD: GET',
+                  ],
+              ],
+          ],
+      ],
+  ];
+
   public function testExample()
   {
     $stack = [];
